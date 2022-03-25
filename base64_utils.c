2b59b4573255dc6d8255688fb2f8a050e793b216
SynAcktraa <harshmetaexploit90@gmail.com>
2022-03-26 04:27:43 +0530

    removed minor bug

--- base64_utils.c
+++ base64_utils.c
@@ -42,7 +42,7 @@ int delete(char*data, int index, int length){
 
 void reverse(char*str){
    
-   int len = 0, temp;
+   int len = 0;
    char*ptr = str;
    while(*ptr != '\0'){
       len++;
@@ -50,7 +50,7 @@ void reverse(char*str){
    }
    int i = 0, j = len-1;
    while(i < j){
-      temp  = *(str+i);
+      int temp  = *(str+i);
       *(str+i) = *(str+j);
       *(str+j) = temp;
       ++i;
