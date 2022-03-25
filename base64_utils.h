302983f8717bdc3910d17dde42216495c2cd8ed9
SynAcktraa <harshmetaexploit90@gmail.com>
2022-03-26 03:25:48 +0530

    updated header file

--- base64_utils.h
+++ base64_utils.h
@@ -3,16 +3,12 @@
 
 //Function declarations
 
-int Strlen(const char* string);
-char *Strcpy(char* destination, char* source);
-void Strcat(char* destination, const char* source);
-int Strcmp(const char* string1, const char* string2);
-int Strncut(char* destination, char* source, int nCut);
 int insert(char*data, int index, int element, int uSize, int tSize);
 int delete(char*data, int index, int len);
-void memreset(char*data, int len);
+void reverse(char*str);
 int power(int base, int exp);
-void decToBin(int dec, char* bin);
+char* decToBin(int dec);
 int binToDec(char* bin);
+char *basename(char const *path);
 
 #endif
