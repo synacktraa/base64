2aa64b6c557072c4b43eea941967329f3818369f
SynAcktraa <harshmetaexploit90@gmail.com>
2022-03-29 22:20:51 +0530

    added comments, kinda documented

--- base64.c
+++ base64.c
@@ -14,6 +14,15 @@
 
 char* get_file_data(char*file, char ch) {
 
+/*
+    Checks if file exists on the system, if yes
+    stores the file size in buffer_len var
+    by evaulating get_filesize function which 
+    retrieves the file size and then reads the
+    file line by line and stores it in buffer 
+    and then concatenate it to data_storage
+    and finally frees the buffer and return data_storage
+*/
     if(!checkIfFileExists(file)) {
         fprintf(stderr, "FileError: can't open %s file.", file);
         putc(ch, stdout);
@@ -39,7 +48,12 @@ void encode(char*data, char*md, char ch, char* out){
 
     int buffer_len;
     char* plaintext;
-
+/*
+    compares md var, if it's '-f' it evaluates
+    get_file_data and gets the file content and stores
+    it in plaintext, else if it's '-i' it duplicates 
+    the data and stores it in plaintext.    
+*/
     if( !strcmp(md, "-f") ) {
         plaintext = get_file_data(data, ch);
 
@@ -49,7 +63,9 @@ void encode(char*data, char*md, char ch, char* out){
     } else plaintext = "";
     buffer_len = Strlen(plaintext);
 
+    // calculates space for base64 encoded string
     int base64_val_space = (0.4*(buffer_len+1))+buffer_len+2;
+    // calculates space for binary dump of input string
     int bin_dump_space = (buffer_len * 8)+1;
 
     char six_bit_bin[7], Ox49_val_bin[9]; 
@@ -64,11 +80,19 @@ void encode(char*data, char*md, char ch, char* out){
 
     for( i=0; plaintext[i] != '\0'; ++i ) {
         label:
+        /*
+            charValidate checks for non-ascii characters
+        */
             if( charValidate(plaintext[i]) == -1 ){
                 fprintf(stderr, "InputError: can't take non-ascii characters.");
                 putc(ch, stdout);
                 exit(1);
             }
+        /*
+            checks for CR(carriage return) [this problem occurs in unix 
+            systems] if present it deletes CR at the current
+            index and proceeds to continue from label.
+        */
             if (plaintext[i] == 13){
                 delete(plaintext, i, Strlen(plaintext));
                 goto label;    
@@ -76,10 +100,16 @@ void encode(char*data, char*md, char ch, char* out){
 
         strcpy(Ox49_val_bin, decToBin(plaintext[i]));
 		while( Strlen(Ox49_val_bin)%8 != 0 ){
-
+        /*
+            checks for 8 bit binary, if not it starts
+            adding zeroes from the 0th index until it's
+            8 bit binary value.
+        */
         	k = insert(Ox49_val_bin, 0, '0', Strlen(Ox49_val_bin), Strlen(Ox49_val_bin)+1);
         	Ox49_val_bin[k] = '\0';
-        }   
+        }
+        //concatenates the 8 bit binary in bin_dump to create
+        //a binary dump which will be manipulated later   
         strcat(bin_dump, Ox49_val_bin);
         memset(Ox49_val_bin, 0, Strlen(Ox49_val_bin));
 
@@ -88,16 +118,24 @@ void encode(char*data, char*md, char ch, char* out){
     free(plaintext);
     
     bin_dump_len = Strlen(bin_dump);
-
     while( bin_dump_len%6 != 0 )
+    /*
+        checks if the length of binary dump is in the
+        multiplication of 6, coz base64 -> 2^6 = 64
+    */
         bin_dump_len = insert(bin_dump, bin_dump_len, '0', bin_dump_len, Strlen(bin_dump)+1);
 
     i = 0, j = 0;   
     while( i[bin_dump]!='\0' ){
-
+    /*
+        moves 6 bits from bin_dump to six_bit_bin,
+        converts the 6 bit binary to decimal and stores
+        it in ascii_val and do some comparisions, then
+        adds accordingly and stores it in base64_val string
+        and increments i by 6.
+    */
         memset(six_bit_bin, 0, Strlen(six_bit_bin));
         memmove(six_bit_bin, bin_dump+i, 6);
-        // six_bit_bin[6] = 0;
 
         int ascii_val = binToDec(six_bit_bin);
 
@@ -119,13 +157,23 @@ void encode(char*data, char*md, char ch, char* out){
     free(bin_dump);
 
     while( Strlen(base64_val)%4 != 0 )
+    /*
+        inserts '=' at the end of the base64 encoded string until
+        the length is in the multiplication of 4.
+    */
         insert(base64_val, Strlen(base64_val), 0x3d, Strlen(base64_val), base64_val_space);
 
     if ( out == NULL ){
+    /*
+        if outfile is not given, print the output
+        in cli/terminal
+    */
         fwrite(base64_val, 1, Strlen(base64_val), stdout);
         putc(ch, stdout);
     } else {
-
+    /*
+        else writes the ouput to the file
+    */
         FILE * fp = fopen(out, "w");
         fputs(base64_val, fp);
     }
@@ -138,7 +186,12 @@ void decode(char*data, char*md, char ch, char* out){
 	int i, j;
     int buffer_len;
     char* base64_data;
-
+/*
+    compares md var, if it's '-f' it evaluates
+    get_file_data and gets the file content and stores
+    it in base64_data, else if it's '-i' it duplicates 
+    the data and stores it in base64_data.    
+*/
     if( !strcmp(md, "-f") ) {
         base64_data = get_file_data(data, ch);
 
@@ -149,7 +202,9 @@ void decode(char*data, char*md, char ch, char* out){
 
     buffer_len = Strlen(base64_data);
     
+    // calculates space for base64 encoded string
     int decData_val_space = (buffer_len+2)-(0.15*buffer_len);
+    // calculates space for binary dump of input string
     int bin_dump_space = (buffer_len * 6)+1;
 
     char Ox49_val_bin[9], byte_bin[9];
@@ -158,6 +213,10 @@ void decode(char*data, char*md, char ch, char* out){
 
 
 	while(*(base64_data+(buffer_len-1)) == 0x3D){
+    /*
+        checks for '=' from the end of the input encoded string
+        and deletes the padding 
+    */
 		buffer_len = delete(base64_data, buffer_len-1, buffer_len);
 	}
 
@@ -172,7 +231,11 @@ void decode(char*data, char*md, char ch, char* out){
             putc(ch, stdout);
             exit(1);
         }
-    
+    /*
+        checks for encoded data simultaneously, then subtracts
+        and evaluated decimal to binary function, then copies it
+        in Ox49_val_bin
+    */
         if( base64_data[i]>='B' && base64_data[i]<='Z' )
             strcpy(Ox49_val_bin, decToBin(base64_data[i]-65));
         else if( base64_data[i]>='a' && base64_data[i]<='z' )
@@ -187,8 +250,13 @@ void decode(char*data, char*md, char ch, char* out){
             strcpy(Ox49_val_bin, "000000");
 
         int k = Strlen(Ox49_val_bin);
-        while( k%6 != 0 )
+        while( k%6 != 0 ){
+        /*
+            checks if the length of binary is in the
+            multiplication of 6, coz base64 -> 2^6 = 64
+        */
             k = insert(Ox49_val_bin, 0, '0', k, sizeof(Ox49_val_bin));
+        }
         
         strcat(bin_dump, Ox49_val_bin);
         memset(Ox49_val_bin, 0, Strlen(Ox49_val_bin));
@@ -196,13 +264,23 @@ void decode(char*data, char*md, char ch, char* out){
     free(base64_data);
     
     int bin_dump_len = Strlen(bin_dump);
-    while( bin_dump_len%8 != 0 )
+    while( bin_dump_len%8 != 0 ){
+    /*
+        checks for 8 bit binary, if not it starts
+        adding zeroes from the 0th index until it's
+        8 bit binary value.
+    */
         bin_dump_len = insert(bin_dump, bin_dump_len , '0', bin_dump_len, bin_dump_space);
+    }
     *(bin_dump+bin_dump_len) = '\0';
 
     i = 0, j = 0;
     while( bin_dump[i]!='\0' ){
-
+    /*
+        moves 1 byte from bin_dump to byte_bin,
+        converts that byte binary to decimal and stores
+        it in decodeData and increments i by 8.
+    */
         memset(byte_bin, 0, Strlen(byte_bin));
         memmove(byte_bin, bin_dump+i, 8);
         byte_bin[8] = 0;
@@ -215,10 +293,16 @@ void decode(char*data, char*md, char ch, char* out){
     free(bin_dump);
 
     if( out == NULL){
+    /*
+        if outfile is not given, print the output
+        in cli/terminal
+    */
         fwrite(decodeData, 1, Strlen(decodeData), stdout);
         putc(ch, stdout);
     } else {
-
+    /*
+        else writes the ouput to the file
+    */
         FILE * fp = fopen(out, "w");
         fputs(decodeData, fp);
     }
@@ -230,6 +314,12 @@ int main(int argc, char* argv[]){
     
     char *store = "", *flag = "", *out = "";
     int ch = 0, i = 0;
+
+    /*
+        checks for operating system, if it detects windows
+        ch is set null, if it's unix/linux ch is set to new
+        line
+    */
     #ifdef _WIN32
         ch = 0;
     #elif __unix__
